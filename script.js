dc895661b3ae88c2327e7d3fbb5548f84925903e
adimidania <ja_adimi@esi.dz>
2024-01-06 13:58:48 +0100

    Added reset and resize features

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('#container')
-
 for(i=0; i<16*16; i++) {
     const element = document.createElement('div')
     container.appendChild(element)
@@ -9,3 +8,13 @@ const elements = document.querySelectorAll('#container div')
 elements.forEach(element => element.addEventListener('mouseover', function(e) {
     element.style.backgroundColor = 'black'
 }))
+
+const reset = document.querySelector('#reset')
+
+
+reset.addEventListener('click', function(e) {
+    elements.forEach(element => element.style.backgroundColor = 'transparent')
+})
+
+
+
