a7e2e9e87fc038119476191c70728a320357a509
adimidania <ja_adimi@esi.dz>
2024-01-06 16:46:45 +0100

    Refactored the JavaScript code

--- script.js
+++ script.js
@@ -1,93 +1,110 @@
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
 function randomizeColor() {
     const chars = '0123456789ABCDEF';
     let color = '#';
     for (i=0; i<6; i++) {
         color += chars.charAt(Math.random()* chars.length)
     }
-    return color
+    return color;
 }
 
 let container = document.querySelector('#container');
 const main = document.querySelector('main');
-let currentColor = '#333333'
-let theme = 'color'
-
-for(i=0; i<16; i++) {
-    const row = document.createElement('div');
-    for(j=0; j<16; j++) {
-        const element = document.createElement('div');
-        row.appendChild(element)
-    }
-    container.appendChild(row);
-}
-
-let elements = document.querySelectorAll('#container div div');
-elements.forEach(element => element.addEventListener('click', function(e) {
-    if (theme == 'color') {
-        element.style.backgroundColor = currentColor;
-    }
-    else if (theme == 'rainbow') {
-        element.style.backgroundColor = randomizeColor();
-    } else if (theme == 'erase') {
-        element.style.backgroundColor = 'transparent';
-    }
-}))
-
 const reset = document.querySelector('#reset');
 const resize = document.querySelector('#resize');
 const rainbow = document.querySelector('#rainbow');
 const erase = document.querySelector('#erase');
 const colorInput = document.querySelector('#color-input');
 
-reset.addEventListener('click', function(e) {
-    elements.forEach(element => element.style.backgroundColor = 'transparent');
+rainbow.addEventListener('click', () => setCurrentMode('rainbow'));
+erase.addEventListener('click', () => setCurrentMode('erase'));
+colorInput.addEventListener('input', (e) => {
+    console.log('Target', e.target.value);
+    setCurrentMode('color');
+    setCurrentColor(e.target.value);
+    console.log(currentColor);
+})
+reset.addEventListener('click', () => {
+    clearGrid();
+});
+resize.addEventListener('click', function(e) {
+    getNewSize();
+    removeGrid();
+    createGrid(currentSize);
+    main.appendChild(container);
 })
 
-resize.addEventListener('click', function(e) {
-    let size = parseInt(prompt("Please enter the number of squares per side (Max. 100)"));
-    if (Number.isInteger(size)) {
-        if (size > 100) {
-            size = 100;
-        }
-    } else {
-        console.log('Not integer');
-        size = 16;
-    }
-    main.removeChild(container);
-    while(container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+function createGrid(size) {
     for(i=0; i<size; i++) {
         const row = document.createElement('div');
         for(j=0; j<size; j++) {
             const element = document.createElement('div');
+            element.addEventListener('click', changeColor)
             row.appendChild(element)
         }
         container.appendChild(row);
     }
-    main.appendChild(container);
-    elements = container.querySelectorAll('#container div div');
-    elements.forEach(element => element.addEventListener('click', function(e) {
-        if (theme == 'color') {
-            element.style.backgroundColor = currentColor;
-        }
-        else if (theme == 'rainbow') {
-            element.style.backgroundColor = randomizeColor();
-        } else if (theme == 'erase') {
+}
+
+function clearGrid() {
+    for(i=0; i<currentSize; i++) {
+        const row = container.children[i];
+        for(j=0; j<currentSize; j++) {
+            const element = row.children[j];
             element.style.backgroundColor = 'transparent';
         }
-    }))
-})
+    } 
+}
 
-rainbow.addEventListener('click', function(e) {
-    theme = 'rainbow';
-})
+function removeGrid() {
+    main.removeChild(container);
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
-erase.addEventListener('click', function(e) {
-    theme = 'erase';
-})
+function getNewSize() {
+    let size = parseInt(prompt("Please enter the number of squares per side (Max. 100)"));
+    if (Number.isInteger(size)) {
+        if (size > 100) {
+            setCurrentSize(100);
+        } else {
+            setCurrentSize(size);
+        }
+    } else {
+        setCurrentSize(16);
+    }
+}
 
-colorInput.addEventListener('input', function(e) {
-    currentColor = e.target.value;
-})
+function changeColor(e) {
+    if (currentMode == 'color') {
+        e.target.style.backgroundColor = currentColor;
+    }
+    else if (currentMode == 'rainbow') {
+        e.target.style.backgroundColor = randomizeColor();
+    } 
+    else {
+        e.target.style.backgroundColor = 'transparent';
+    }
+}
 
+createGrid(currentSize);
